6be384f3b5381f0c5b77108b4485529a6db7f9ca
VaibhavSuryawanshiSurya <er.vaibhavsuryawanshi@gmail.com>
2023-02-12 02:17:30 +0530

    Modify labs

--- Labs/Python-Practice/time_from_12.py
+++ Labs/Python-Practice/time_from_12.py
@@ -5,20 +5,21 @@ when both are within a 12-hour cycle. """
 
 
 def cal_seconds(hours, minutes, seconds):
+    """Calculate the total seconds of given input time since the last time the clock 'reached 12'"""
+
     total_seconds  = ((hours*60) + minutes)*60 + seconds
     return total_seconds
 
 def main():
-    # print(time.localtime(time.time()))
     hours = int(input("Enter Time in\nhours    :"))
     minutes = int(input("minutes  :"))
     seconds = int(input("seconds  :"))
 
-    if hours>=0 and hours<=12 and minutes>=0 and minutes<=60 and seconds>=0 and seconds<60:
+    if hours>=0 and hours<12 and minutes>=0 and minutes<60 and seconds>=0 and seconds<60:
         total_seconds = cal_seconds(hours, minutes, seconds)
         print("the total number of seconds is", total_seconds)
     else:
-        print("Please enter valid inputs (hours, minutes, seconds) in 12-hour cycle :")
+        print("Please enter valid inputs (hours, minutes, seconds) in 12-hour cycle")
         main()
     
 if __name__ == "__main__":
