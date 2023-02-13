38b7bf43bc8c306429002b3c93b3a63b0dd8b2bb
VaibhavSuryawanshiSurya <er.vaibhavsuryawanshi@gmail.com>
2023-02-13 16:01:21 +0530

    Modify labs

---
+++ Labs/Python-Practice/is_palindrome_word.py
@@ -0,0 +1,19 @@
+""" Write a program that determines if a word is a palindrome. """
+
+
+def palindrome(input_str):
+    """ a word, phrase, or sequence that reads the same backwards as forwards. """
+    
+    lower_str = input_str
+    if lower_str == lower_str[::-1]:
+        print(f"Given string '{input_str}' is a Palindrome. ")
+    else:
+        print(f"Given string '{input_str}' is not a Palindrome. ")
+
+def main():
+
+    input_str = input("Enter the word:     ")
+    palindrome(input_str)
+
+if __name__ == "__main__":
+    main()
