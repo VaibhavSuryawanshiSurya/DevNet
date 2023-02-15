340112b322ff5eacf65eeaa55a9937449fdc4816
VaibhavSuryawanshiSurya <er.vaibhavsuryawanshi@gmail.com>
2023-02-15 17:07:34 +0530

    Modify Labs

---
+++ Labs/Python-Practice/separate_words.py
@@ -0,0 +1,17 @@
+""" Write a program that receives a sentence and separates the words by spaces. 
+For example, if I receive the text "I love programming in Python", it should 
+return "I" "love" "programming" "in" "Python" in a list. """
+
+
+def separate_words(input_str):
+    words_list = input_str.split()
+    return words_list
+
+def main():
+
+    input_str = input("Enter the sentence:     ")
+    words_list = separate_words(input_str)
+    print(words_list)
+
+if __name__ == "__main__":
+    main()
